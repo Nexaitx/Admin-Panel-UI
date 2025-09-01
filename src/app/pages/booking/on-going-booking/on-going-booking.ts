e06aaacc90bccee6d24e91ddd8a817e5f19b1bff
rukhsana1827 <rukhsana1827asif@gmail.com>
2025-09-01 21:57:02 +0530

    updated UI booking

--- src/app/pages/booking/on-going-booking/on-going-booking.ts
+++ src/app/pages/booking/on-going-booking/on-going-booking.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, Input, SimpleChanges } from '@angular/core';
+import { Component, inject, Input, SimpleChanges, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { API_URL, ENDPOINTS } from '../../../core/const';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-on-going-booking',
@@ -12,23 +16,40 @@ import { API_URL, ENDPOINTS } from '../../../core/const';
     CommonModule,
     MatTableModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
     MatButtonModule
   ],
   templateUrl: './on-going-booking.html',
   styleUrl: './on-going-booking.scss'
 })
 export class OnGoingBooking {
-@Input() booking: any;
+  @Input() booking: any;
   http = inject(HttpClient);
   dataSource = new MatTableDataSource<any>;
 
   // Updated: Include all new fields in the columns to display
   columnsToDisplay = ['id', 'loginSelfiePath', 'staffName', 'staffPhoneNumber', 'userName', 'userPhoneNumber', 'loginTime', 'logoutTime', 'latitude', 'longitude', 'status', 'action'];
-
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   isLoading = false;
 
   ngOnInit() { }
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['booking'] && changes['booking'].currentValue) {
       this.fetchData();
--- src/app/pages/booking/up-coming-booking/up-coming-booking.ts
+++ src/app/pages/booking/up-coming-booking/up-coming-booking.ts
@@ -1,17 +1,27 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, Input, SimpleChanges } from '@angular/core';
+import { Component, inject, Input, SimpleChanges, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { API_URL, ENDPOINTS } from '../../../core/const';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-up-coming-booking',
   imports: [CommonModule,
     MatTableModule,
     MatIconModule,
-    MatButtonModule],
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule,
+    DatePipe,
+    MatButtonModule
+  ],
   templateUrl: './up-coming-booking.html',
   styleUrl: './up-coming-booking.scss'
 })
@@ -19,10 +29,29 @@ export class UpComingBooking {
   @Input() booking: any;
   http = inject(HttpClient);
   dataSource = new MatTableDataSource<any>();
-  columnsToDisplay = ['id', 'loginSelfiePath', 'staffName', 'staffPhoneNumber', 'userName', 'userPhoneNumber', 'loginTime', 'logoutTime', 'latitude', 'longitude', 'status', 'action'];
+  columnsToDisplay = ['bookingId', 'startDate', 'endDate', 'status', 'userName', 'userPhone', 'staffName', 'staffPhone', 'duties', 'price'];
   isLoading = false;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  ngOnInit() { }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['booking'] && changes['booking'].currentValue) {
