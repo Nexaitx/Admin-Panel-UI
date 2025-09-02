567834a7d96eef26cc7aca203f6b6f9cd9e3503b
rukhsana1827 <rukhsana1827asif@gmail.com>
2025-09-02 18:52:51 +0530

    updated login and signup UI

--- src/app/layouts/main-ui/main-ui.ts
+++ src/app/layouts/main-ui/main-ui.ts
@@ -4,10 +4,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { Signup } from '../../pages/signup/signup';
 
 @Component({
   selector: 'app-main-ui',
   imports: [Login,
+    Signup,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
@@ -18,6 +20,7 @@ import { CommonModule } from '@angular/common';
 })
 export class MainUi {
 isMenuOpen = false;
+authUI: 'login' | 'signup' = 'signup';
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
