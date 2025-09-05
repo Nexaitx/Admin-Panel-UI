3453d2a82178e081d4785a7957630decbc636bb8
rukhsana1827 <rukhsana1827asif@gmail.com>
2025-09-05 23:53:44 +0530

    added charts and graphs

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,16 +1,58 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { API_URL, ENDPOINTS } from '../../core/const';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import {
+  ApexNonAxisChartSeries,
+  ApexPlotOptions,
+  ApexChart,
+  NgApexchartsModule,
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexDataLabels,
+  ApexYAxis,
+  ApexLegend,
+  ApexStroke,
+  ApexXAxis,
+  ApexFill,
+  ApexTooltip
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  labels: string[];
+  plotOptions: ApexPlotOptions;
+};
+
+export type ChartOptionClientStaff = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  fill: ApexFill;
+  tooltip: ApexTooltip;
+  stroke: ApexStroke;
+  legend: ApexLegend;
+};
+
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
   imports: [RouterLink,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    NgApexchartsModule
   ],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss'
+  styleUrls: ['./dashboard.scss']
 })
 
 export class Dashboard {
@@ -22,26 +64,128 @@ export class Dashboard {
   doctors: any;
   dieticians: any;
   pharmacist: any;
+  userProfile = JSON.parse(localStorage.getItem('userProfile') || '{}');
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions!: ChartOptions;
+  public chartOptionClientStaff!: ChartOptionClientStaff;
+  constructor() {
+    this.chartOptions = {
+      series: [4, 5, 7, 8],
+      chart: {
+        height: 200,
+        type: "radialBar"
+      },
+      plotOptions: {
+        radialBar: {
+          dataLabels: {
+            name: {
+              fontSize: "22px"
+            },
+            value: {
+              fontSize: "16px"
+            },
+            total: {
+              show: true,
+              label: "Total",
+              formatter: function (w) {
+                return "30";
+              }
+            }
+          }
+        }
+      },
+      labels: ["Doctors", "Dieticians", "Physiotherapist", "Psychiatrist"]
+    };
 
-  ngOnInit() {
-    this.getData();
-  }
+    this.chartOptionClientStaff = {
+      series: [
+        {
+          name: "Net Profit",
+          data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+        },
+        {
+          name: "Revenue",
+          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+        },
+        {
+          name: "Free Cash Flow",
+          data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+        }
+      ],
+      chart: {
+        type: "bar",
+        height: 350
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: "55%"
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ["transparent"]
+      },
+      legend: {
+        show: true,
+        position: 'top',
+        horizontalAlign: 'right'
+      },
+      xaxis: {
+        categories: [
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct"
+        ]
+      },
+      yaxis: {
+        title: {
+          text: "$ (thousands)"
+        }
+      },
+      fill: {
+        opacity: 1
+      },
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return "$ " + val + " thousands";
+          }
+        }
+      }
+    };
+}
 
-  getData() {
-    // get dietplans
-    this.http.get(API_URL + ENDPOINTS.GET_DIETPLAN).subscribe((res: any) => {
-      this.dietPlans = res;
-    });
+ngOnInit() {
+  console.log(this.userProfile);
+  this.getData();
+}
 
-    // get dieticians
-    this.http.get(API_URL + ENDPOINTS.GET_ACCOUNT_BY_ROLE + '/Dietician').subscribe((res: any) => {
-      this.dieticians = res;
-    });
+getData() {
+  // get dietplans
+  this.http.get(API_URL + ENDPOINTS.GET_DIETPLAN).subscribe((res: any) => {
+    this.dietPlans = res;
+  });
 
-    // get Doctors
-    this.http.get(API_URL + ENDPOINTS.GET_ACCOUNT_BY_ROLE + '/Admin').subscribe((res: any) => {
-      this.doctors = res;
-    });
+  // get dieticians
+  this.http.get(API_URL + ENDPOINTS.GET_ACCOUNT_BY_ROLE + '/Dietician').subscribe((res: any) => {
+    this.dieticians = res;
+  });
 
-  }
+  // get Doctors
+  this.http.get(API_URL + ENDPOINTS.GET_ACCOUNT_BY_ROLE + '/Admin').subscribe((res: any) => {
+    this.doctors = res;
+  });
+
+}
 }
